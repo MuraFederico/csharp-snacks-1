dfd024ea40ab57cad4894f9f6ca9443d79545569
MuraFederico <federicomura98@gmail.com>
2022-06-27 14:38:16 +0200

    snack 4

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//snack 4
+
+int sum2 = 0;
+float avg = 0f;
+int a = 0;
+
+for (int i = 2; i <= 10; i++)
+{
+    a += 1;
+    sum2 = sum2 + i;
+}
+
+avg = sum2 / a;
+Console.WriteLine(sum2);
+Console.WriteLine(avg);
+
+
+return;
+
+/*snack 3*/
 
 int sum = 0;
 
