3604887adda0160b67c5ef8d9e03c2dfaaec435f
MuraFederico <federicomura98@gmail.com>
2022-06-27 14:32:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int sum = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("inserisci un numero: ");
+    int num = Int32.Parse(Console.ReadLine());
+    sum = sum + num;
+}
+
+Console.WriteLine("la somma dei numeri e': " + sum);
